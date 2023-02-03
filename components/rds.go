438de3a096da9996f3620e3afac970cdf5031d7e
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2023-02-03 20:35:16 +0500

    rds refactor

---
+++ components/rds.go
@@ -0,0 +1,47 @@
+package components
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+type RDS struct {
+	Id               string
+	Region           string
+	AllocatedStorage string
+	DBName           string
+	Engine           string
+	EngineVersion    string
+	Instance         string
+	Username         string
+	Password         string
+}
+
+func (r RDS) CreateComponent() {
+	src := "tf/" + r.Region + "/rds-" + r.Id + ".tf"
+	txtFile, err := os.Open(src)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully Opened rds.tf")
+	defer txtFile.Close()
+	body, err := ioutil.ReadAll(txtFile)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	raw := strings.Split(string(body), "=")
+	var final string
+	final += raw[0][2:] + "= " + r.AllocatedStorage
+	final += raw[1] + "= \"" + r.DBName + "\""
+	final += raw[2] + "= \"" + r.Engine + "\""
+	final += raw[3] + "= \"" + r.EngineVersion + "\""
+	final += raw[4] + "= \"" + r.Instance + "\""
+	final += raw[5] + "= \"" + r.Username + "\""
+	final += raw[6] + "= \"" + r.Password + "\""
+	final += raw[7] + "= true" + raw[8][:len(raw[8])-2]
+
+	ioutil.WriteFile(src, []byte(final), 0644)
+}
--- main.go
+++ main.go
@@ -1,42 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"log"
-	"os"
 	"strings"
 
 	"github.com/ahmed-deftoner/tf-generator/components"
 	"github.com/ahmed-deftoner/tf-generator/utils"
 )
-
-func createRDS(node map[string]utils.Node, id string, region string) {
-	src := "tf/" + region + "/rds-" + id + ".tf"
-	txtFile, err := os.Open(src)
-	key := "RDS#" + id
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("Successfully Opened rds.tf")
-	defer txtFile.Close()
-	body, err := ioutil.ReadAll(txtFile)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	raw := strings.Split(string(body), "=")
-	var final string
-	final += raw[0][2:] + "= " + node[key].Properties["allocated_storage"]
-	final += raw[1] + "= \"" + node[key].Properties["db_name"] + "\""
-	final += raw[2] + "= \"" + node[key].Properties["engine"] + "\""
-	final += raw[3] + "= \"" + node[key].Properties["engine_version"] + "\""
-	final += raw[4] + "= \"" + node[key].Properties["instance_class"] + "\""
-	final += raw[5] + "= \"" + node[key].Properties["username"] + "\""
-	final += raw[6] + "= \"" + node[key].Properties["password"] + "\""
-	final += raw[7] + "= true" + raw[8][:len(raw[8])-2]
-
-	ioutil.WriteFile(src, []byte(final), 0644)
-}
 
 func main() {
 	graph := utils.DecodeJSON("graph.json")
@@ -70,10 +39,32 @@ func main() {
 		} else if raw[0] == "RDS" {
 			if raw[1] != "" {
 				utils.MoveFile(raw[1], n.Region, "rds")
-				createRDS(graph.Nodes, raw[1], n.Region)
+				r := components.RDS{
+					Id:               raw[1],
+					AllocatedStorage: n.Properties["allocated_storage"],
+					Region:           n.Region,
+					DBName:           n.Properties["db_name"],
+					Engine:           n.Properties["engine"],
+					EngineVersion:    n.Properties["engine_version"],
+					Instance:         n.Properties["instance_class"],
+					Username:         n.Properties["username"],
+					Password:         n.Properties["password"],
+				}
+				components.Component.CreateComponent(r)
 			} else {
 				utils.MoveFile("", n.Region, "rds")
-				createRDS(graph.Nodes, "", n.Region)
+				r := components.RDS{
+					Id:               raw[1],
+					AllocatedStorage: n.Properties["allocated_storage"],
+					Region:           n.Region,
+					DBName:           n.Properties["db_name"],
+					Engine:           n.Properties["engine"],
+					EngineVersion:    n.Properties["engine_version"],
+					Instance:         n.Properties["instance_class"],
+					Username:         n.Properties["username"],
+					Password:         n.Properties["password"],
+				}
+				components.Component.CreateComponent(r)
 			}
 		}
 		utils.EditProvider(n.Region)
